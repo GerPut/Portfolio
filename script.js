24ff0b2c248a5c53e6ab1762ec48245c2e0e4a52
Gerhard <gerhard.putter@yahoo.com>
2021-07-17 07:30:13 +0200

    Added functionality of cursor and follower.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//Cursor
+
+gsap.set('.follower', { xPercent: -50, yPercent: -50 });
+gsap.set('.cursor', { xPercent: -50, yPercent: -50 });
+
+var follow = document.querySelector('.follower')
+var cursor = document.querySelector('.cursor')
+
+window.addEventListener('mousemove', e => {
+    gsap.to(cursor, 0.2, { x: e.clientX, y: e.clientY });
+    gsap.to(follow, 0.9, { x: e.clientX, y: e.clientY });
+})
+
 
 //Variables
 
