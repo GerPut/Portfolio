07f991cfed7e632dc76e90c61f52da4fc5e6e7aa
Gerhard <gerhard.putter@yahoo.com>
2021-07-18 07:03:16 +0200

    Gsap animation work done for home page.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//Cursor
-
+//Cursor Animation
 gsap.set('.follower', { xPercent: -50, yPercent: -50 });
 gsap.set('.cursor', { xPercent: -50, yPercent: -50 });
 
@@ -11,9 +10,14 @@ window.addEventListener('mousemove', e => {
     gsap.to(follow, 0.9, { x: e.clientX, y: e.clientY });
 })
 
+//Hello fly in animation
+gsap.from('.hello', { duration: 1, x: '-100vw', delay: 0.5, ease: 'power.in' })
+gsap.from('.cog-container', { duration: 1, x: '200vw', delay: 0.5, ease: 'power.in' })
 
-//Variables
+//Spinning links navbar animation
+gsap.fromTo('.navnavlink', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, delay: 2.5, opacity: 1, scale: 1, rotation: 0 })
 
+//Variables Navbar Menu
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 
